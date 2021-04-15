45d2ab82c3ef7cc8eff98a80172db632c29ab72f
EdgarASteffen <51060151+EdgarASteffen@users.noreply.github.com>
2021-04-14 21:47:04 -0300

    Add files via upload

---
+++ Atividade8/Aluno.js
@@ -0,0 +1,12 @@
+var Aluno = new Object();
+Aluno.ra = '123456';
+Aluno.nome = 'Antonio da Silva';
+
+var Aluno = {};
+Aluno.ra = '123456';
+Aluno.nome = 'Antonio da Silva';
+
+var Aluno = {
+    ra: '123456',
+    nome: 'Antonio da Silva'
+};
