52a8e4742142a25942f4d17eb6292b80d77d9c40
EdgarASteffen <51060151+EdgarASteffen@users.noreply.github.com>
2021-04-28 22:23:29 -0300

    Add files via upload

---
+++ Atividade10/janelas.js
@@ -0,0 +1,11 @@
+function janelaAberta(imagem) {
+    imagem.src = "janelaaberta.png";
+}
+
+function janelaFechada(imagem) {
+    imagem.src = "janelafechada.png";
+}
+
+function janelaQuebrada(imagem) {
+    imagem.src = "janelaquebra.png"
+}
